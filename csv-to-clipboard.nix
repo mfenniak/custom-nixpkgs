f6218b776d6d736144133467ace290fa53e22a37
Mathieu Fenniak <mathieu@fenniak.net>
2022-09-19 20:41:13 -0600

    Convert to a flake

--- csv-to-clipboard.nix
+++ csv-to-clipboard.nix
@@ -1,41 +1,41 @@
-{
-  pkgs ? import (fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/998f0f7924198b2460458728de59fe738997f28e.tar.gz";
-    sha256 = "sha256:1iph9yydfk7a7r2b8137v96d9v6jrnbspbvra30kkcrjm1c1gin0";
-  }) {}
-}:
+# {
+#   pkgs ? import (fetchTarball {
+#     url = "https://github.com/NixOS/nixpkgs/archive/998f0f7924198b2460458728de59fe738997f28e.tar.gz";
+#     sha256 = "sha256:1iph9yydfk7a7r2b8137v96d9v6jrnbspbvra30kkcrjm1c1gin0";
+#   }) {}
+# }:
 
-let
-  desktopFile = pkgs.makeDesktopItem {
-    name = "csv-to-clipboard";
-    exec = "@out@/bin/csv-to-clipboard %f";
-    desktopName = "csv-to-clipboard";
-    mimeTypes = [ "text/csv" ];
-  };
-in
-pkgs.rustPlatform.buildRustPackage rec {
-  pname = "csv-to-clipboard";
-  version = "1.1.0";
+# let
+#   desktopFile = pkgs.makeDesktopItem {
+#     name = "csv-to-clipboard";
+#     exec = "@out@/bin/csv-to-clipboard %f";
+#     desktopName = "csv-to-clipboard";
+#     mimeTypes = [ "text/csv" ];
+#   };
+# in
+# pkgs.rustPlatform.buildRustPackage rec {
+#   pname = "csv-to-clipboard";
+#   version = "1.1.0";
 
-  src = pkgs.fetchFromGitHub {
-    owner = "mfenniak";
-    repo = pname;
-    rev = version;
-    sha256 = "sha256-Aag741LAD/C7ajJmIs7bsCMg2mPfkYVIAdJ71ZIlAog=";
-  };
+#   src = pkgs.fetchFromGitHub {
+#     owner = "mfenniak";
+#     repo = pname;
+#     rev = version;
+#     sha256 = "sha256-Aag741LAD/C7ajJmIs7bsCMg2mPfkYVIAdJ71ZIlAog=";
+#   };
 
-  cargoSha256 = "sha256-6TNgMnVjfx/LLNkOwUx6Gs4x/fTFa5fw+CrhWv2vNFE=";
+#   cargoSha256 = "sha256-6TNgMnVjfx/LLNkOwUx6Gs4x/fTFa5fw+CrhWv2vNFE=";
 
-  nativeBuildInputs = [
-    pkgs.python310
-  ];
+#   nativeBuildInputs = [
+#     pkgs.python310
+#   ];
 
-  buildInputs = [
-    pkgs.xorg.libxcb
-  ];
+#   buildInputs = [
+#     pkgs.xorg.libxcb
+#   ];
 
-  postInstall = ''
-    mkdir -p $out/share/applications
-    substituteAll ${desktopFile}/share/applications/csv-to-clipboard.desktop $out/share/applications/csv-to-clipboard.desktop
-  '';
-}
+#   postInstall = ''
+#     mkdir -p $out/share/applications
+#     substituteAll ${desktopFile}/share/applications/csv-to-clipboard.desktop $out/share/applications/csv-to-clipboard.desktop
+#   '';
+# }
---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "csv-to-clipboard nix flake";
+
+  outputs = { self, nixpkgs }: {
+
+    # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default =
+      with import nixpkgs { system = "x86_64-linux"; };
+      let
+        desktopFile = pkgs.makeDesktopItem {
+          name = "csv-to-clipboard";
+          exec = "@out@/bin/csv-to-clipboard %f";
+          desktopName = "csv-to-clipboard";
+          mimeTypes = [ "text/csv" ];
+        };
+      in
+      pkgs.rustPlatform.buildRustPackage rec {
+        pname = "csv-to-clipboard";
+        version = "1.1.0";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "mfenniak";
+          repo = pname;
+          rev = version;
+          sha256 = "sha256-Aag741LAD/C7ajJmIs7bsCMg2mPfkYVIAdJ71ZIlAog=";
+        };
+
+        cargoSha256 = "sha256-6TNgMnVjfx/LLNkOwUx6Gs4x/fTFa5fw+CrhWv2vNFE=";
+
+        nativeBuildInputs = [
+          pkgs.python310
+        ];
+
+        buildInputs = [
+          pkgs.xorg.libxcb
+        ];
+
+        postInstall = ''
+          mkdir -p $out/share/applications
+          substituteAll ${desktopFile}/share/applications/csv-to-clipboard.desktop $out/share/applications/csv-to-clipboard.desktop
+        '';
+      };
+  };
+}
