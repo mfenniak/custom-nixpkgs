92e6625b479a9f3686d95af1878b1fed3ce6f848
Mathieu Fenniak <mathieu@fenniak.net>
2022-12-19 20:55:17 -0700

    Add a harness for testing prometheus exporters to the custom-nixpkgs flake; introduce the broken podman exporter (containers/prometheus-podman-exporter#78)

--- flake/flake.nix
+++ flake/flake.nix
@@ -8,6 +8,11 @@
   outputs = { self, nixpkgs }: {
     overlays.default = self: super: {
       csv-to-clipboard = self.callPackage ../csv-to-clipboard.nix {};
+      prometheus-podman-exporter = self.callPackage ../prometheus-podman-exporter.nix {};
+    };
+
+    nixosModules.default = { ... }: {
+      imports = [ ../prometheus/exporters.nix ];
     };
   };
 }
---
+++ prometheus/exporters/podman.nix
@@ -0,0 +1,26 @@
+{ config, lib, pkgs, options }:
+
+with lib;
+
+let
+  cfg = config.services.prometheus-custom.exporters.podman;
+in
+{
+  port = 9882;
+  serviceOpts = {
+    serviceConfig = {
+      DynamicUser = false;
+      StateDirectory = "prometheus-podman-exporter";
+      WorkingDirectory = "/var/lib/prometheus-podman-exporter";
+      # ProtectHome = lib.mkForce "tmpfs";
+      # WorkingDirectory = "$RUNTIME_DIRECTORY";
+      # RuntimeDirectory = "prometheus-podman-exporter";
+      # StateDirectory = "prometheus-podman-exporter";
+      ExecStart = ''
+        ${pkgs.prometheus-podman-exporter}/bin/prometheus-podman-exporter \
+          --web.listen-address ${cfg.listenAddress}:${toString cfg.port} \
+          ${concatStringsSep " \\\n  " cfg.extraFlags}
+      '';
+    };
+  };
+}
