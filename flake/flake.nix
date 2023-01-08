5efa2be78fc0e1b5e8e9859c0238e34894db300b
Mathieu Fenniak <mathieu@fenniak.net>
2023-01-07 18:42:05 -0700

    add detect-reboot-needed nixos module

--- flake/flake.nix
+++ flake/flake.nix
@@ -1,18 +1,18 @@
 {
   description = "mfenniak's custom-nixpkgs flake";
 
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-  };
-
-  outputs = { self, nixpkgs }: {
+  outputs = { self }: {
     overlays.default = self: super: {
       csv-to-clipboard = self.callPackage ../csv-to-clipboard.nix {};
       prometheus-podman-exporter = self.callPackage ../prometheus-podman-exporter.nix {};
     };
 
-    nixosModules.default = { ... }: {
+    nixosModules.prometheus-exporter-podman = { ... }: {
       imports = [ ../prometheus/exporters.nix ];
+    };
+
+    nixosModules.detect-reboot-needed = { ... }: {
+      imports = [ ../detect-reboot-needed.nix ];
     };
   };
 }
