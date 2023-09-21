bcb8640d4928606f7b64dbb57a4ec727ab766960
Mathieu Fenniak <mathieu@fenniak.net>
2023-09-20 20:00:36 -0600

    move overlays out of flakeutils as well

--- flake/flake.nix
+++ flake/flake.nix
@@ -10,14 +10,6 @@
     (flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system}; in
     {
-      overlays.default = self: super: {
-        csv-to-clipboard = self.callPackage ../csv-to-clipboard.nix {};
-        prometheus-podman-exporter = self.callPackage ../prometheus-podman-exporter.nix {};
-        plasma-applet-display-profile-switcher = self.callPackage ../plasma-applet-display-profile-switcher.nix {};
-        dungeondraft = self.callPackage ../dungeondraft.nix {};
-        wonderdraft = self.callPackage ../wonderdraft.nix {};
-      };
-
       packages = rec {
         lib-rpi-rgb-led-matrix = pkgs.callPackage ../lib-rpi-rgb-led-matrix.nix {};
         python-bdfparser = python: (pkgs.callPackage ../python-bdfparser.nix {
@@ -31,6 +23,14 @@
         });
       };
     })) // {
+      overlays.default = self: super: {
+        csv-to-clipboard = self.callPackage ../csv-to-clipboard.nix {};
+        prometheus-podman-exporter = self.callPackage ../prometheus-podman-exporter.nix {};
+        plasma-applet-display-profile-switcher = self.callPackage ../plasma-applet-display-profile-switcher.nix {};
+        dungeondraft = self.callPackage ../dungeondraft.nix {};
+        wonderdraft = self.callPackage ../wonderdraft.nix {};
+      };
+
       nixosModules.prometheus-exporter-podman = { ... }: {
         imports = [ ../prometheus/exporters.nix ];
       };
